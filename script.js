2840fcf2fad700f27077055dce805cb3c5a9ba79
Gulshan-koondhar <153608541+Gulshan-koondhar@users.noreply.github.com>
2024-09-08 13:52:27 +0500

    Milestone 1 2 & 3

--- script.js
+++ script.js
@@ -2,9 +2,78 @@ var hide1 = document.querySelector(".hide1");
 var hide = document.querySelector(".hide");
 var edSkill = document.querySelector(".ed-skills");
 var projectList = document.querySelector(".project-list");
+var modal = document.querySelector("#modal");
 hide1 === null || hide1 === void 0 ? void 0 : hide1.addEventListener("click", function (e) {
     projectList === null || projectList === void 0 ? void 0 : projectList.classList.toggle("active");
 });
 hide === null || hide === void 0 ? void 0 : hide.addEventListener("click", function (e) {
     edSkill === null || edSkill === void 0 ? void 0 : edSkill.classList.toggle("active");
 });
+var generateCV = function () {
+    var nameField = document.querySelector("#nameField");
+    var nameT = document.querySelector("#nameT");
+    if (nameField) {
+        if (nameT) {
+            nameT.innerText = nameField.value;
+        }
+    }
+    var emailField = document.querySelector("#emailField");
+    var emailT = document.querySelector("#emailT");
+    if (emailField) {
+        if (emailT) {
+            emailT.innerText = emailField.value;
+        }
+    }
+    var contactField = document.querySelector("#contactField");
+    var contactT = document.querySelector("#contactT");
+    if (contactField) {
+        if (contactT) {
+            contactT.innerText = contactField.value;
+        }
+    }
+    var jobTitle = document.querySelector("#jobTitle");
+    var jobT = document.querySelector("#jobT");
+    if (jobTitle) {
+        if (jobT) {
+            jobT.innerText = jobTitle.value;
+        }
+    }
+    var objField = document.querySelector("#objField");
+    var objT = document.querySelector("#objT");
+    if (objField) {
+        if (objT) {
+            objT.innerText = objField.value;
+        }
+    }
+    var weField = document.querySelector(".weField");
+    var weT = document.querySelector("#weT");
+    if (weField) {
+        if (weT) {
+            weT.innerText = weField.value;
+        }
+    }
+    var skField = document.querySelector(".skField");
+    var skillT = document.querySelector("#skillT");
+    if (skField) {
+        if (skillT) {
+            skillT.innerText = skField.value;
+        }
+    }
+    var langField = document.querySelector(".langField");
+    var langT = document.querySelector("#langT");
+    if (langField) {
+        if (langT) {
+            langT.innerText = langField.value;
+        }
+    }
+    var proField = document.querySelector(".proField");
+    var proT = document.querySelector("#proT");
+    if (proField) {
+        if (proT) {
+            proT.innerText = proField.value;
+        }
+    }
+    if (modal) {
+        modal.style.display = "none";
+    }
+};
--- script.ts
+++ script.ts
@@ -3,6 +3,8 @@ const hide: HTMLElement | null = document.querySelector(".hide");
 const edSkill: HTMLElement | null = document.querySelector(".ed-skills");
 const projectList: HTMLElement | null = document.querySelector(".project-list");
 
+const modal:HTMLElement | null = document.querySelector("#modal")
+
 hide1?.addEventListener("click", (e: MouseEvent): void => {
     projectList?.classList.toggle("active");
 });
@@ -10,3 +12,76 @@ hide1?.addEventListener("click", (e: MouseEvent): void => {
 hide?.addEventListener("click", (e: MouseEvent): void => {
     edSkill?.classList.toggle("active");
 });
+
+
+const generateCV:()=>void=()=>{
+        const nameField: HTMLInputElement | null = document.querySelector("#nameField");
+        const nameT :HTMLElement | null =document.querySelector("#nameT")
+        if (nameField) {
+            if(nameT){
+                nameT.innerText = nameField.value;
+            }
+        }
+        const emailField: HTMLInputElement | null = document.querySelector("#emailField");
+        const emailT :HTMLElement | null =document.querySelector("#emailT")
+        if (emailField) {
+            if(emailT){
+                emailT.innerText = emailField.value;
+            }
+        }
+        const contactField: HTMLInputElement | null = document.querySelector("#contactField");
+        const contactT :HTMLElement | null =document.querySelector("#contactT")
+        if (contactField) {
+            if(contactT){
+                contactT.innerText = contactField.value;
+            }
+        }
+        const jobTitle: HTMLInputElement | null = document.querySelector("#jobTitle");
+        const jobT :HTMLElement | null =document.querySelector("#jobT")
+        if (jobTitle) {
+            if(jobT){
+                jobT.innerText = jobTitle.value;
+            }
+        }
+        const objField: HTMLInputElement | null = document.querySelector("#objField");
+        const objT :HTMLElement | null =document.querySelector("#objT")
+        if (objField) {
+            if(objT){
+                objT.innerText = objField.value;
+            }
+        }
+        const weField: HTMLInputElement | null = document.querySelector(".weField");
+        const weT :HTMLElement | null =document.querySelector("#weT")
+        if (weField) {
+            if(weT){
+                weT.innerText = weField.value;
+            }
+        }
+        const skField: HTMLInputElement | null = document.querySelector(".skField");
+        const skillT :HTMLElement | null =document.querySelector("#skillT")
+        if (skField) {
+            if(skillT){
+                skillT.innerText = skField.value;
+            }
+        }
+        const langField: HTMLInputElement | null = document.querySelector(".langField");
+        const langT :HTMLElement | null =document.querySelector("#langT")
+        if (langField) {
+            if(langT){
+                langT.innerText = langField.value;
+            }
+        }
+        const proField: HTMLInputElement | null = document.querySelector(".proField");
+        const proT :HTMLElement | null =document.querySelector("#proT")
+        if (proField) {
+            if(proT){
+                proT.innerText = proField.value;
+            }
+        }
+
+        
+        if(modal){
+
+            modal.style.display = "none"
+        }
+}
